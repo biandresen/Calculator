fb6ac78bd461306050828f3a37d933f9a7a0c7c7
birando <birger1@live.com>
2024-03-20 11:38:13 +0100

    feat: add function operate each mathematical function

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// function operate(operator, num1, num2) {
+//   if (operator === "+") return addNumbers(num1, num2);
+//   else if (operator === "-") return subtractNumbers(num1, num2);
+//   else if (operator === "*") return multiplyNumbers(num1, num2);
+//   else if (operator === "/") return divideNumbers(num1, num2);
+//   else return "ERROR";
+// }
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return addNumbers(num1, num2);
+    case "-":
+      return subtractNumbers(num1, num2);
+    case "*":
+      return multiplyNumbers(num1, num2);
+    case "/":
+      return divideNumbers(num1, num2);
+    default:
+      return "ERROR";
+  }
+}
+
 function addNumbers(num1, num2) {
   return num1 + num2;
 }
