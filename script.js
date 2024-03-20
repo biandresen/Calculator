80251ad41873fe471a414d3c92136b55ae451d3a
birando <birger1@live.com>
2024-03-20 11:15:49 +0100

    feat: add functions add, sub, multiply, divide

---
+++ script.js
@@ -0,0 +1,15 @@
+function addNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+function subtractNumbers(num1, num2) {
+  return num1 - num2;
+}
+
+function multiplyNumbers(num1, num2) {
+  return num1 * num2;
+}
+
+function divideNumbers(num1, num2) {
+  return num1 / num2;
+}
